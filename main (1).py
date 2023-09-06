76e7f021458a6c0946cdd19f0826afbc7efc39d2
22cs47 <144208521+22cs47@users.noreply.github.com>
2023-09-06 12:49:10 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,13 @@
+#find leap year
+# User enters the year
+year = int(input("Enter Year: "))
+
+# Leap Year Check
+if year % 4 == 0 and year % 100 != 0:
+  print(year, "is a Leap Year")
+elif year % 100 == 0:
+  print(year, "is not a Leap Year")
+elif year % 400 == 0:
+  print(year, "is a Leap Year")
+else:
+  print(year, "is not a Leap Year")
